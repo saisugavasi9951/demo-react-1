1c2070c325eea389819a12237d47d92ba4a3b6d5
saikiransugavasi <saikiran.s@position2.com>
2024-05-08 18:32:05 +0530

    added slider

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const MySlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    scroll: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+  return (
+    <Slider {...settings}>
+      <div>
+        <h3>Slide 1</h3>
+      </div>
+      <div>
+        <h3>Slide 2</h3>
+      </div>
+      <div>
+        <h3>Slide 3</h3>
+      </div>
+      {/* Add more slides here */}
+    </Slider>
+  );
+}
+
+export default MySlider;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Slider from '../../Components/Slider/Slider';
 
 const Home = () => {
   return (
     <div>
       <h2>Welcome to my website!</h2>
       <p>This is the home page.</p>
+      <Slider />
     </div>
   );
 }
