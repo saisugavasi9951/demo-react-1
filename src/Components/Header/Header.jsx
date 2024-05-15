7bb58ee372b7c798656d682a5eb5b109b192720f
saikiransugavasi <saikiran.s@position2.com>
2024-05-15 13:36:02 +0530

    added offcanvas

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Button, Offcanvas, OffcanvasHeader, OffcanvasBody } from 'react-bootstrap';
 
 function Header() {
+  // State to manage the visibility of the Offcanvas
+  const [show, setShow] = useState(false);
+
+  // Toggle function to open and close the Offcanvas
+  const toggleOffcanvas = () => setShow(!show);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -15,15 +23,25 @@ function Header() {
             <Nav.Link href="/contact-us">Contact Us</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
+
+            <div>
+              <Button variant="primary" onClick={toggleOffcanvas}>
+                Open
+              </Button>
+              <Offcanvas show={show} onHide={toggleOffcanvas} placement="end">
+                <OffcanvasHeader closeButton>
+                  <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </OffcanvasHeader>
+                <OffcanvasBody>
+                  <strong>This is the Offcanvas body.</strong>
+                </OffcanvasBody>
+              </Offcanvas>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
